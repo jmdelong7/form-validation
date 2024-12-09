37bbe8d9c744d39ce724af25a8b2630443c762d6
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-12-08 16:57:01 -0800

    Validation obj with form checks
    Object with methods to check form inputs:
    - email
    - zip code
    - password
    - confirm password

--- src/index.js
+++ src/index.js
@@ -12,10 +12,36 @@ import countries from "./countries.js";
   })
 })();
 
-function checkEmailValidity(email) {
-  const split1 = email.split('@');
-  if (split1.length !== 2) return false;
-  const split2 = split1[1].split('.');
-  if (split2.length !== 2) return false;
-  return true;
+const validation = {
+  general: (value) => {
+    // Checks for blank, null, and whitespace (space, tab, linebreak).
+    if (value === '' || value === null || /\s/.test(value) === true){
+      return false;
+    }
+    return true;
+  },
+  email: (email) => {
+    const split1 = email.split('@');
+    if (split1.length !== 2) return false;
+    const split2 = split1[1].split('.');
+    if (split2.length !== 2) return false;
+    return true;  
+  },
+  zip: (zip) => {
+    if (zip.includes('-') || zip.includes('.') || zip.length < 5 || zip.length > 5){
+      return false;
+    };
+    return true;
+  },
+  passw1: (passw) => {
+    const safe = /^[A-Za-z0-9!@#$%^&*()_+\-=,.?]{8,}$/;
+    if (passw.length < 8 || passw.length > 30 || !safe.test(passw)){
+      return false;
+    }
+    return true;
+  },
+  passw2: (passw1, passw2) => {
+    return passw1 === passw2 ? true : false;
+  }
 }
+
